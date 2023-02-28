44d45bc8b5470d054b996ccfdf8969527eb02f3d
arktal <101146331+gpollet@users.noreply.github.com>
2023-02-28 12:59:59 +0100

    Improved responsive for smaller screens Improved userScore inner text

--- frontend/src/Components/UserScore.tsx
+++ frontend/src/Components/UserScore.tsx
@@ -1,14 +1,22 @@
-import { Label, Pie, PieChart, ResponsiveContainer, Text } from "recharts";
+import { Pie, PieChart } from "recharts";
 import { formatedScore } from "../types.js";
 
-const UserScore = ({ score, height, width }: { score: formatedScore[], height?: number, width?: number }):JSX.Element => {
-	const pieRadius = 90;
+const UserScore = ({
+	score,
+	height,
+	width,
+}: {
+	score: formatedScore[];
+	height?: number;
+	width?: number;
+}): JSX.Element => {
+	//  Changes how far/close the circle is from the edges of the container
+	const pieRadius = height ? height / 2.6 : 250;
 	const startAngle = 210;
 	/**
 	 * Calculates the value of the <Pie> endAngle needed for the circle to accurately display user's progress
 	 */
 	const userProgress = score[0].score * 100;
-	const labelContent = `${userProgress}% de votre objectif`;
 	const percentToAngle = () => {
 		// Since we know 3.6° = 1%, calculates at how many degrees the end angle must be to actually display the user's progress in %
 		const progressAngle = userProgress * 3.6;
@@ -22,6 +30,33 @@ const UserScore = ({ score, height, width }: { score: formatedScore[], height?: 
 				width={width}
 				height={height}
 			>
+				<text
+					x={width ? width / 3 : undefined}
+					y={height ? height / 6 : undefined}
+					textAnchor="end"
+					fontSize="1em"
+				>
+					Score
+				</text>
+				<text
+					x={height ? height / 2 : undefined}
+					y={width ? width / 2 : undefined}
+					textAnchor="middle"
+					alignmentBaseline="middle"
+					fontWeight={700}
+					fontSize="2em"
+				>
+					{`${userProgress}%`}
+				</text>
+				<text
+					textAnchor="middle"
+					alignmentBaseline="middle"
+					x={height ? height / 2 : undefined}
+					y={width ? width / 2 + 26 : undefined}
+					
+				>
+					{`de votre objectif`}
+				</text>
 				<Pie
 					data={score}
 					dataKey={"score"}
@@ -30,21 +65,8 @@ const UserScore = ({ score, height, width }: { score: formatedScore[], height?: 
 					fill="#FF0000"
 					startAngle={startAngle}
 					endAngle={percentToAngle()}
-				>
-					<Label
-						width={75}
-						content={
-							<Text
-								textAnchor="middle"
-								verticalAnchor="middle"
-								x={258 / 2}
-								y={263 / 2}
-							>
-								{labelContent}
-							</Text>
-						}
-					/>
-				</Pie>
+					cornerRadius={5}
+				></Pie>
 			</PieChart>
 		</>
 	);
